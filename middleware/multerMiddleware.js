32b2f3ab4610eef9ac5a1b946ce166da1726ba1f
VrushaliHaldankar01 <vrushalihaldankar01@gmail.com>
2024-08-14 04:35:44 -0400

    added code

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -10,7 +10,10 @@ const serviceAccount = {
   project_id: process.env.PROJECT_ID,
   private_key_id: process.env.PRIVATE_KEY_ID,
   private_key: process.env.PRIVATE_KEY,
-  // private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+
+  private_key: process.env.PRIVATE_KEY
+    ? process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
+    : undefined,
   client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
   auth_uri: process.env.AUTH_URI,
